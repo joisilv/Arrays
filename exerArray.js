8c958bb341d2d14d96447caf4b24f7f34b3d902a
joisilv <joice-silva1996@hotmail.com>
2019-10-11 21:30:47 -0300

    objeto

--- exerArray.js
+++ exerArray.js
@@ -18,10 +18,10 @@ const arr = [2,4,6,3,4,5,6]
 
 console.log (arr.indexOf(3));*/
 
-/* exer3 forma2 de fazer 
+exer3 forma2 de fazer 
 const arr = [0,3,2,1,4,5,6]
 for (let i = 0; i < arr.length; i++){
     if (arr[i] == 3)
     console.log (i);
-    }*/
+    }
     
